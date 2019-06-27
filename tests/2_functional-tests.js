47faec493c45e52f1c811290b1acc8db53b55b32
minggas <fasfora@gmail.com>
2019-06-26 21:09:15 -0300

    fix: Tests for GET route

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,10 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
 var chaiHttp = require("chai-http");
 var chai = require("chai");
@@ -43,12 +43,12 @@ suite("Functional Tests", function() {
           done();
         });
     });
-    
+
     test("1 stock with like", function(done) {
       chai
         .request(server)
         .get("/api/stock-prices")
-        .query({ stock: "goog", like: true })
+        .query({ stock: "msft", like: true })
         .end(function(err, res) {
           assert.equal(res.status, 200);
           assert.isObject(res.body.stockData, "Not a object");
@@ -67,17 +67,17 @@ suite("Functional Tests", function() {
             res.body.stockData.likes,
             "likes need to be a number"
           );
-          assert.equal(res.body.stockData.stock, "GOOG");
+          assert.equal(res.body.stockData.stock, "MSFT");
           assert.equal(res.body.stockData.likes, 1);
           done();
         });
-      });
-      
+    });
+
     test("1 stock with like again (ensure likes arent double counted)", function(done) {
       chai
         .request(server)
         .get("/api/stock-prices")
-        .query({ stock: "goog", like: true })
+        .query({ stock: "msft", like: true })
         .end(function(err, res) {
           assert.equal(res.status, 200);
           assert.isObject(res.body.stockData, "Not a object");
@@ -96,24 +96,118 @@ suite("Functional Tests", function() {
             res.body.stockData.likes,
             "likes need to be a number"
           );
-          assert.equal(res.body.stockData.stock, "GOOG");
+          assert.equal(res.body.stockData.stock, "MSFT");
           assert.equal(res.body.stockData.likes, 1);
           done();
         });
-      });
-      
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
-      });
-      
-      test('2 stocks', function(done) {
-        
-      });
-      
-      test('2 stocks with like', function(done) {
-        
-      });
-      
     });
 
+    test("2 stocks", function(done) {
+      chai
+        .request(server)
+        .get("/api/stock-prices")
+        .query({ stock: ["msft", "goog"], like: false })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body.stockData, "Not a array");
+          assert.property(res.body.stockData[0], "stock", "No stock property");
+          assert.property(res.body.stockData[0], "price", "No price property");
+          assert.property(
+            res.body.stockData[0],
+            "rel_likes",
+            "No likes property"
+          );
+          assert.isString(
+            res.body.stockData[0].stock,
+            "stock need to be a string"
+          );
+          assert.isString(
+            res.body.stockData[0].price,
+            "price need to be a string"
+          );
+          assert.isNumber(
+            res.body.stockData[0].rel_likes,
+            "likes need to be a number"
+          );
+          assert.equal(res.body.stockData[0].stock, "MSFT");
+          assert.equal(res.body.stockData[0].rel_likes, 1);
+          assert.property(res.body.stockData[1], "stock", "No stock property");
+          assert.property(res.body.stockData[1], "price", "No price property");
+          assert.property(
+            res.body.stockData[1],
+            "rel_likes",
+            "No likes property"
+          );
+          assert.isString(
+            res.body.stockData[1].stock,
+            "stock need to be a string"
+          );
+          assert.isString(
+            res.body.stockData[1].price,
+            "price need to be a string"
+          );
+          assert.isNumber(
+            res.body.stockData[1].rel_likes,
+            "likes need to be a number"
+          );
+          assert.equal(res.body.stockData[1].stock, "GOOG");
+          assert.equal(res.body.stockData[1].rel_likes, -1);
+          done();
+        });
+    });
+
+    test("2 stocks with like", function(done) {
+      chai
+        .request(server)
+        .get("/api/stock-prices")
+        .query({ stock: ["msft", "goog"], like: true })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body.stockData, "Not a array");
+          assert.property(res.body.stockData[0], "stock", "No stock property");
+          assert.property(res.body.stockData[0], "price", "No price property");
+          assert.property(
+            res.body.stockData[0],
+            "rel_likes",
+            "No likes property"
+          );
+          assert.isString(
+            res.body.stockData[0].stock,
+            "stock need to be a string"
+          );
+          assert.isString(
+            res.body.stockData[0].price,
+            "price need to be a string"
+          );
+          assert.isNumber(
+            res.body.stockData[0].rel_likes,
+            "likes need to be a number"
+          );
+          assert.equal(res.body.stockData[0].stock, "MSFT");
+          assert.equal(res.body.stockData[0].rel_likes, 0);
+          assert.property(res.body.stockData[1], "stock", "No stock property");
+          assert.property(res.body.stockData[1], "price", "No price property");
+          assert.property(
+            res.body.stockData[1],
+            "rel_likes",
+            "No likes property"
+          );
+          assert.isString(
+            res.body.stockData[1].stock,
+            "stock need to be a string"
+          );
+          assert.isString(
+            res.body.stockData[1].price,
+            "price need to be a string"
+          );
+          assert.isNumber(
+            res.body.stockData[1].rel_likes,
+            "likes need to be a number"
+          );
+          assert.equal(res.body.stockData[1].stock, "GOOG");
+          assert.equal(res.body.stockData[1].rel_likes, 0);
+          done();
+        });
+    });
+  });
 });
